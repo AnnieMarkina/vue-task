b944660d94932055ab94b34cbdc3e237c1e988e8
AnnieMarkina <anniemurkina@gmail.com>
2020-07-02 17:27:50 +0300

    adaptiveness added

--- src/components/BannerImgs/BannerImgs.vue
+++ src/components/BannerImgs/BannerImgs.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="main__imgs">
-    <div class="main__img--left">
-      <img src="../../assets/images/image2.png" alt="Ink Lingerie" />
-    </div>
-
     <div class="main__img--center">
+      <div id="left">
+        <img src="../../assets/images/image2.png" alt="Ink Lingerie" />
+      </div>
       <img id="center" src="../../assets/images/image1.png" alt="Ink Lingerie" />
       <img id="right" src="../../assets/images/image3.png" alt="Ink Lingerie" />
     </div>
